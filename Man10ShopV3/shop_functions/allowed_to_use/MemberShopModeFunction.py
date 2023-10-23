252cf4358b83c14247f6b326a232d93aa2fda3aa
sho <shojabon@gmail.com>
2023-10-24 04:16:40 +0900

    メンバーショップのisを==に変更

--- Man10ShopV3/shop_functions/allowed_to_use/MemberShopModeFunction.py
+++ Man10ShopV3/shop_functions/allowed_to_use/MemberShopModeFunction.py
@@ -15,7 +15,7 @@ class MemberShopModeFunction(ShopFunction):
     def is_allowed_to_use_shop(self, order: OrderRequest) -> bool:
         if self.is_enabled():
             user_permission = self.shop.permission_function.get_permission(order.player)
-            if user_permission is "NONE" or user_permission is "BANNED":
+            if user_permission == "NONE" or user_permission == "BANNED":
                 order.player.warn_message("このメンバー限定ショップを使用することができません")
                 return False
         return True
